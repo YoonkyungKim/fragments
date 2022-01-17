5968972da4deea4a2ae68c00ef6010967866df05
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2022-01-17 13:48:02 -0500

    Initial work on express app and server

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// To gracefully shutdown our server (i.e., wait until current connections are finished before shutting down)
+const stoppable = require('stoppable');
+
+// Get our logger instance
+const logger = require('./logger');
+
+// Get our express app instance
+const app = require('./app');
+
+// Get the desired port from the process environment. Default to `8080`
+const port = parseInt(process.env.PORT || 8080, 10);
+
+// Start a server listening on this port
+const server = stoppable(
+  app.listen(port, () => {
+    // Log a message that the server has started, and which port it's using
+    logger.info({ port }, `Server started`);
+  })
+);
+
+// Export our server instance so other parts of our code can access it if necessary
+module.exports = server;
