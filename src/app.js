c5a7be84bb22a95c60cf6851e3d8b95f28dc8000
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2022-01-22 19:25:56 -0500

    Define routes and fragments API endpoints

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 
-//version and author from our package.json file
-const { version, author } = require('../package.json');
-
 const logger = require('./logger');
 const pino = require('pino-http')({
   // Use our default logger instance, which is already configured
@@ -27,25 +24,12 @@ app.use(cors());
 // Use gzip/deflate compression middleware
 app.use(compression());
 
-// Define a simple health check route. If the server is running
-// We'll respond with a 200 OK. If not, the server isn't healthy.
-app.get('/', (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader('Cache-Control', 'no-cache');
-
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    author,
-    githubUrl: 'https://github.com/YoonkyungKim/fragments',
-    version,
-  });
-});
+// Define our routes
+app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
-  res.status(404).status.json({
+  res.status(404).json({
     status: 'error',
     error: {
       message: 'not found',
---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+// Version and author from package.json
+const { version, author } = require('../../package.json');
+
+// Create a router that we can use to mount our API
+const router = express.Router();
+
+/**
+ * Expose all of our API routes on /v1/* to include an API version.
+ */
+router.use(`/v1`, require('./api'));
+
+const app = express();
+
+/**
+ * Define a simple health check route. If the server is running
+ * We'll respond with a 200 OK. If not, the server isn't healthy.
+ */
+app.get('/', (req, res) => {
+    // Clients shouldn't cache this response (always request it fresh)
+    // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
+    res.setHeader('Cache-Control', 'no-cache');
+  
+    // Send a 200 'OK' response with info about our repo
+    res.status(200).json({
+        status: 'ok',
+        author,
+        githubUrl: 'https://github.com/YoonkyungKim/fragments',
+        version,
+    });
+});
+
+module.exports = router;
