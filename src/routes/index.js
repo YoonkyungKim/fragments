cd145ea02e74da5bd3679e214b257661da19da54
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2022-01-22 20:58:16 -0500

    Fix 404 error

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,21 +3,23 @@ const express = require('express');
 // Version and author from package.json
 const { version, author } = require('../../package.json');
 
+// Our authorization middleware
+const { authenticate } = require('../authorization');
+
 // Create a router that we can use to mount our API
 const router = express.Router();
 
 /**
  * Expose all of our API routes on /v1/* to include an API version.
+ * Protect them all so you have to be authenticated in order to access.
  */
-router.use(`/v1`, require('./api'));
-
-const app = express();
+router.use(`/v1`, authenticate(), require('./api'));
 
 /**
  * Define a simple health check route. If the server is running
  * We'll respond with a 200 OK. If not, the server isn't healthy.
  */
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     // Clients shouldn't cache this response (always request it fresh)
     // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
     res.setHeader('Cache-Control', 'no-cache');
