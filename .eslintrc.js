0abf9f64e8bc7faa37e210a6c333dc9c6de42ce6
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2022-01-28 21:43:03 -0500

    Config Jest for testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
+    // To configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
   },
   extends: 'eslint:recommended',
   parserOptions: {
