55898e9606dd9d3dad06d2279c8fa241781be5be
Yoonkyung Kim <ykim268@myseneca.ca>
2022-02-14 20:56:47 -0500

    Add post route with its test and add raw body parser

---
+++ tests/unit/post.test.js
@@ -0,0 +1,50 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('POST /v1/fragments', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () => request(app).post('/v1/fragments').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
+
+  // User with valid username/password pair should be able to create a plain text fragment
+  // responses include all necessary and expected properties (id, created, type, etc),
+  // and these values match what you expect for a given request (e.g., size and type)
+  test('authenticated users create a plain text fragment, response include expected properties', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send("aa");
+    const body = JSON.parse(res.text);
+    expect(res.statusCode).toBe(201);
+    expect(body.status).toBe('ok');
+    expect(body.fragment.type).toMatch(/text\/plain+/);
+    expect(Object.keys(body.fragment)).toEqual(['id', 'ownerId', 'created', 'updated', 'type', 'size']);
+    expect(body.fragment.size).toEqual(2);
+  });
+
+  //responses include a Location header with a URL to GET the fragment
+  test('response include a Location header with a URL to GET the fragment', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send("This is fragment");
+    expect(res.statusCode).toBe(201);
+    expect(res.headers.location).toEqual(`${process.env.API_URL}/v1/fragments/${JSON.parse(res.text).fragment.id}`);
+  });
+
+  //trying to create a fragment with an unsupported type errors as expected
+  test('get unsupported type error', () => 
+    request(app)
+      .post('/v1/fragments')
+      .set('Content-Type', 'image/png')
+      .auth('user1@email.com', 'password1')
+      .send("aa")
+      .expect(415)
+  );
+});
