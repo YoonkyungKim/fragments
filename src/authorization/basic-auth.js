fdbbe039bbf5845a431d6e60b35400ddfdf50c41
Yoonkyung Kim <ykim268@myseneca.ca>
2022-02-14 21:11:05 -0500

    Add hash.js with its test and middleware and update auth files to use this middleware

--- src/authorization/basic-auth.js
+++ src/authorization/basic-auth.js
@@ -2,21 +2,22 @@
 // https://github.com/http-auth/http-auth-passport
 
 const auth = require('http-auth');
-const passport = require('passport');
 const authPassport = require('http-auth-passport');
+
+const authorize = require('./authorize-middleware');
 
 // We expect HTPASSWD_FILE to be defined.
 if (!process.env.HTPASSWD_FILE) {
-    throw new Error('missing expected env var: HTPASSWD_FILE');
+  throw new Error('missing expected env var: HTPASSWD_FILE');
 }
 
 module.exports.strategy = () =>
-    // For our Passport authentication strategy, we'll look for a
-    // username/password pair in the Authorization header.
-    authPassport(
-        auth.basic({
-            file: process.env.HTPASSWD_FILE,
-        })
-    );
+// For our Passport authentication strategy, we'll look for a
+// username/password pair in the Authorization header.
+  authPassport(
+    auth.basic({
+      file: process.env.HTPASSWD_FILE,
+    })
+  );
 
-module.exports.authenticate = () => passport.authenticate('http', { session: false });
+module.exports.authenticate = () => authorize('http');
