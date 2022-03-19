0089b9a08b75b894c772fd4fa5ddc80355e7d901
Yoonkyung Kim <ykim268@myseneca.ca>
2022-03-19 00:45:34 -0400

    Add GET /fragments/:id/info route and its test

---
+++ src/routes/api/get-by-id-info.js
@@ -0,0 +1,33 @@
+/**
+ * GET /fragments/:id/info
+ * Allows the authenticated user to get (i.e., read) the metadata for one of their existing fragments with the specified id
+ * If no such fragment exists, returns an HTTP 404 with an appropriate error message
+ */
+
+const logger = require('../../logger');
+const { Fragment } = require('../../model/fragment');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+
+module.exports = async (req, res) => {
+  // logger.debug("req.query in get-by-id-info: " + JSON.stringify(req.query));
+  // logger.debug(`owner id and id: ${req.user}, ${req.params.id}`);
+
+  // Any await'ed call needs to have the error case handled so your server doesn't crash
+  try {
+    const fragment = await Fragment.byId(req.user, req.params.id);
+    
+    logger.debug(`get by id req.params: ${JSON.stringify(req.params)}`);
+
+    // If the id does not represent a known fragment, returns an HTTP 404 with an appropriate error message.
+    if (!fragment) {
+      return res.status(404).json(createErrorResponse(404, "No fragment with this id"));
+    }
+
+    res.status(200).json(createSuccessResponse({
+      fragment: fragment,
+    }));
+  } catch (e) {
+    res.status(500).json(createErrorResponse(500, e));
+  }  
+}
+  
---
+++ tests/unit/get-by-id-info.test.js
@@ -0,0 +1,46 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('GET /v1/fragments/:id/info', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () => request(app).get('/v1/fragments/randomid/info').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app).get('/v1/fragments/randomid/info').auth('invalid@email.com', 'incorrect_password').expect(401));
+
+  // Using a valid username/password pair should give a success result with fragment data with given id
+  test('authenticated users get fragment data with the given id', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is fragment');
+    const id = (JSON.parse(postRes.text)).fragment.id;
+    const fragment = (JSON.parse(postRes.text)).fragment;
+
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}/info`)
+      .auth('user1@email.com', 'password1');
+
+    expect(getRes.statusCode).toBe(200);
+    expect(getRes.body.status).toBe('ok');
+    expect(getRes.body.fragment).toEqual(fragment);
+  });
+
+  // No fragment with the given id
+  test('no fragments with the given id returns 404 error', async () => {
+    await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is fragment');
+
+    const getRes = await request(app)
+      .get(`/v1/fragments/randomid/info`)
+      .auth('user1@email.com', 'password1');
+
+    expect(getRes.statusCode).toBe(404);
+  });
+});
