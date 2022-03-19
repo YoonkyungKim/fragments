63a7adeeefd17192cb23034302396a746b88a6e0
Yoonkyung Kim <ykim268@myseneca.ca>
2022-03-19 00:40:41 -0400

    Add error cases and type conversion to GET /fragments/:id with test cases

--- tests/unit/get-by-id.test.js
+++ tests/unit/get-by-id.test.js
@@ -22,7 +22,51 @@ describe('GET /v1/fragments/:id', () => {
     const getRes = await request(app)
       .get(`/v1/fragments/${id}`)
       .auth('user1@email.com', 'password1');
+
     expect(getRes.statusCode).toBe(200);
-    expect(getRes.body).toEqual('This is fragment');
+    expect(getRes.text).toEqual('This is fragment');
+  });
+
+  // No fragment with the given id
+  test('no fragments with the given id returns 404 error', async () => {
+    const getRes = await request(app)
+      .get(`/v1/fragments/randomid`)
+      .auth('user1@email.com', 'password1');
+
+    expect(getRes.statusCode).toBe(404);
+  });
+
+  // If the extension used represents an unknown or unsupported type, or if the fragment cannot be converted to this type, 
+  // an HTTP 415 error is returned instead, with an appropriate message. For example, a plain text fragment cannot be returned as a PNG.
+  test('if fragment cannot be converted to the extension type used, returns 404 error', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user2@email.com', 'password2')
+      .set('Content-Type', 'text/plain')
+      .send('This is fragment');
+    const id = (JSON.parse(postRes.text)).fragment.id;
+
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}.png`)
+      .auth('user2@email.com', 'password2');
+
+    expect(getRes.statusCode).toBe(415);
+  });
+
+  // convert md to html
+  test('markdown data can be converted to html, user can get converted result by specifying extension', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user3@email.com', 'password3')
+      .set('Content-Type', 'text/markdown')
+      .send('# This is fragment');
+    const id = (JSON.parse(postRes.text)).fragment.id;
+
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('user3@email.com', 'password3');
+    console.log("getres text:"+getRes.text);
+    expect(getRes.statusCode).toBe(200);
+    expect(getRes.headers["content-type"]).toEqual("text/html; charset=utf-8");
   });
 });
