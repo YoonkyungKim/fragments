b28efbde5b4141ada927c77e07a14e66f9ec07f3
Yoonkyung Kim <ykim268@myseneca.ca>
2022-04-19 16:07:09 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -46,8 +46,8 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // We may already have an error response we can use, but if not, use a generic
   // 500 server error and message
-  const status = err?.statusCode || 500;
-  const message = err?.message || 'unable to process request';
+  const status = err?.statusCode || err?.error?.code || 500;
+  const message = err?.message || err?.error?.message || 'unable to process request';
 
   // If this is a server error, log something so we can see what's going on
   if (status > 499) {
